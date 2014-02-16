ae6dd1cd8e0d21f9c5b4f7f110b9276ef79befdc
techs <dbittner1@hotmail.com>
2014-02-15 22:20:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,67 @@
-----
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Title</title>
+  <link rel="icon" href="img/favicon.png" type="image/png">
+  <link rel="stylesheet" href="css/app.css">
+  <script src="bower_components/jquery/jquery.js"></script>
+  <script src="bower_components/modernizr/modernizr.js"></script>
+</head>
+
+<body>
+
+	<header class="region1"><!-- headers -->
+		<div class="row">
+			<div class="large-12 columns">
+
+				<ul class="inline-list left">
+					<li><a href="#0" id="tour">Take a tour</a></li>
+				</ul>
+
+			</div>
+		</div>
+  </header>
+  
+  
+    <div id="second"></div>
+    
+    <br>
+    <br>
+    <br><br><br /><br><br /><br>
+    
+    <br><br /><br><br />
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br><br />
+    <br>
+    <br>
+    <div id="third"></div>
+	
+  <ol class="joyride-list" data-joyride>
+    <li data-id="tour" data-text="Next" data-options="tip_location:bottom">
+      <p>start</p>
+    </li>
+    <li data-id="second" data-class="custom so-awesome" data-text="Next">
+      <h4>second</h4>
+      <p>welcome</p>
+    </li>
+    <li data-id="third" data-button="Next" data-options="tip_location:top;tip_animation:fade">
+      <h4>third</h4>
+      <p>more descriptions</p>
+    </li>
+    <li data-button="End">
+      <h4>The end?</h4>
+      <p>thanks for visiting</p>
+    </li>
+  </ol>
+
+  <script src="bower_components/foundation/js/foundation.min.js"></script>
+  <script src="js/app.js"></script>
+  
+</body>
+</html>
